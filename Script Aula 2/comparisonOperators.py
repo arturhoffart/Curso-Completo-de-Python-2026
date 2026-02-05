049ad8392175609b151699ccada2c4b8210c4dc4
Artur <artur.hoffart@hotmail.com>
2026-02-05 15:01:11 -0300

    add comparison operators and for loop

---
+++ Script Aula 2/comparisonOperators.py
@@ -0,0 +1,9 @@
+#working with multiple comparison operators
+
+valor = input("Enter a value: ")
+valor = int(valor)
+
+if 20 <= valor < 40:
+    print("Product accepted")
+else:
+    print("Product rejected")
---
+++ Script Aula 2/repetitionFor.py
@@ -0,0 +1,7 @@
+# for loop (loopping)
+
+#imprimir numeros que o usuario digitar
+number = int(input("Digite um número: "))
+for n in range(number):
+    print(n)    
+    
