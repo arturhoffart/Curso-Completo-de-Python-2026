23029900e1acfcfa366dd03f4f1ca693224503f9
Artur <artur.hoffart@hotmail.com>
2026-02-05 18:25:32 -0300

    operadores ternarios

---
+++ Script Aula 2/OperadorTernario.py
@@ -0,0 +1,11 @@
+# ternary operators (Operadores Ternários)
+
+idade = 10
+'''
+if idade >= 16:
+    print('Voto permitido')
+else:
+    print('Voto não permitido')
+'''
+resultado = 'Voto permidito' if idade >= 16 else 'Voto não permitido'   
+print(resultado)
